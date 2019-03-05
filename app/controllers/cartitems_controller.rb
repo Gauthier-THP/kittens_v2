f06e056691276ee7c39591165e2f5d873fb2fc21
gauthier <gauthier.sayarath@gmail.com>
2019-03-05 18:36:24 +0100

    ajax delete done

--- app/controllers/cartitems_controller.rb
+++ app/controllers/cartitems_controller.rb
@@ -7,17 +7,19 @@ class CartitemsController < ApplicationController
 
   	Cartitem.create(item_id: @item_id, cart_id: @cart_id)
 
+		redirect_to root_path
   end
-
-
 
   def destroy
 
   	@cart = Cart.find(params[:cart_id])
   	@cartitem = Cartitem.find(params[:id])
-  	@cartitem.destroy
+		@cartitem.destroy
+		
+		respond_to do |format|
+			format.js
+			format.html { redirect_to cart_path(@cart) }
+		end
 
-  	redirect_to cart_path(@cart)
-    
   end
 end
