c0aeaa836c67f3fafc40037538161beaa7d58cfd
gauthier <gauthier.sayarath@gmail.com>
2019-03-06 11:55:56 +0100

    mailer progress, merging order creation

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,10 @@ class Order < ApplicationRecord
     belongs_to :user
     has_many :orderitems
     has_many :items, through: :orderitems
+
+    after_create :admin_order_email_send
+
+    def admin_order_email_send
+        AdminMailer.admin_order_email(self).deliver_now
+    end
 end
