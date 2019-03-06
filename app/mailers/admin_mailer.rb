62178296d9ed2e2d373dbf8ac98fa08e2b88b09d
gauthier <gauthier.sayarath@gmail.com>
2019-03-06 14:32:12 +0100

    before sendgrid

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,11 +1,11 @@
 class AdminMailer < ApplicationMailer
   default from: 'no-reply@monsite.fr'
  
-  def admin_order_email(admin)
-    @admin = admin 
+  def admin_order_email(order)
+    @order = order 
 
-    @url  = 'http://monsite.fr/login' 
 
-    mail(to: "azerty966@yopmail.com", subject: 'Bienvenue chez nous !') 
+
+    mail(to: "azerty966@yopmail.com", subject: 'New order') 
   end
 end
